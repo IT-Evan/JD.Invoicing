05d6c500ec8a377562f85e19cd6b8cd2f1e7f4eb
IT-Evan <Evan.net@163.com>
2019-09-18 16:16:13 +0800

    AddPurchase

---
+++ src/JD.Invoicing.Core/Entitys/Data/Customer.cs
@@ -0,0 +1,107 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace JD.Invoicing.Entitys
+{
+    public class Customer : Entity<int>, IHasCreationTime
+    {
+        public Customer()
+        {
+            this.Code = string.Empty;
+            this.Name = string.Empty;
+            this.PinyinCode = string.Empty;
+            this.Address = string.Empty;
+            this.Contact = string.Empty;
+            this.Phone = string.Empty;
+            this.Wechat = string.Empty;
+            this.Area = string.Empty;
+            this.CustomerType = null;
+            this.Remarks = string.Empty;
+            this.Status = null;
+            this.CreateDate = null;
+            this.CreateName = string.Empty;
+            this.UpdateDate = null;
+            this.UpdateName = string.Empty;
+            this.CreationTime = Clock.Now;
+        }
+
+        /// <summary>
+        /// 客商编号
+        /// </summary>
+        [StringLength(50)]
+        public string Code { get; set; }
+        /// <summary>
+        /// 客商名称
+        /// </summary>
+        [StringLength(150)]
+        public string Name { get; set; }
+        /// <summary>
+        /// 拼音码
+        /// </summary>
+        [StringLength(150)]
+        public string PinyinCode { get; set; }
+        /// <summary>
+        /// 地址
+        /// </summary>
+        [StringLength(200)]
+        public string Address { get; set; }
+        /// <summary>
+        /// 联系人
+        /// </summary>
+        [StringLength(50)]
+        public string Contact { get; set; }
+        /// <summary>
+        /// 联系电话
+        /// </summary>
+        [StringLength(50)]
+        public string Phone { get; set; }
+        /// <summary>
+        /// 微信
+        /// </summary>
+        [StringLength(50)]
+        public string Wechat { get; set; }
+        /// <summary>
+        /// 所属区域
+        /// </summary>
+        [StringLength(50)]
+        public string Area { get; set; }
+        /// <summary>
+        /// 客商类别(1:客户, 2:厂商)
+        /// </summary>
+        public int? CustomerType { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [StringLength(200)]
+        public string Remarks { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int? Status { get; set; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        public DateTime? CreateDate { get; set; }
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        [StringLength(50)]
+        public string CreateName { get; set; }
+        /// <summary>
+        /// 修改日期
+        /// </summary>
+        public DateTime? UpdateDate { get; set; }
+        /// <summary>
+        /// 修改人
+        /// </summary>
+        [StringLength(50)]
+        public string UpdateName { get; set; }
+
+        public DateTime CreationTime { get; set; }
+    }
+}
---
+++ src/JD.Invoicing.Core/Entitys/Operation/PurchaseOrder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JD.Invoicing.Entitys
+{
+    public class PurchaseOrder
+    {
+        /// <summary>
+        /// 采购订单编号
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        /// <summary>
+        /// 单据编号
+        /// </summary>
+        [StringLength(45)]
+        public string OrderID { get; set; }
+        /// <summary>
+        /// 单据日期
+        /// </summary>
+        public DateTime? OrderDate { get; set; }
+        /// <summary>
+        /// 制单人日期
+        /// </summary>
+        public DateTime? CreateDate { get; set; }
+        /// <summary>
+        /// 制单人
+        /// </summary>
+        [StringLength(45)]
+        public string CreateName { get; set; }
+        /// <summary>
+        /// 采购员ID
+        /// </summary>
+        public int CGNameID { get; set; }
+        /// <summary>
+        /// 采购员
+        /// </summary>
+        [StringLength(45)]
+        public string CGName { get; set; }
+        /// <summary>
+        /// 供应商id
+        /// </summary>
+        public int Sup_id { get; set; }
+        /// <summary>
+        /// 供应商
+        /// </summary>
+        [StringLength(145)]
+        public string SupName { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [StringLength(200)]
+        public string BZ { get; set; }
+        /// <summary>
+        /// 完成状态 0未完成  1已完成
+        /// </summary>
+        public int? Status { get; set; }
+        /// <summary>
+        /// 审核状态 0未审核 1已审核
+        /// </summary>
+        public int? SHStatus { get; set; }
+        /// <summary>
+        /// 审核日期
+        /// </summary>
+        public DateTime? SHDate { get; set; }
+        /// <summary>
+        /// 审核人
+        /// </summary>
+        [StringLength(45)]
+        public string SHName { get; set; }
+
+        public virtual List<PurchaseOrderMX> PurchaseOrderMX { get; set; }
+
+    }
+}
